e73d6de686895d8305ef04d77e53081a10073864
Rafael Leal <rafaelealdias@gmail.com>
2018-07-07 14:20:04 -0300

    More cleanup

--- project.clj
+++ project.clj
@@ -5,13 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [io.pedestal/pedestal.service "0.5.4"]
-
-                 ;; Remove this line and uncomment one of the next lines to
-                 ;; use Immutant or Tomcat instead of Jetty:
                  [io.pedestal/pedestal.jetty "0.5.4"]
-                 ;; [io.pedestal/pedestal.immutant "0.5.4"]
-                 ;; [io.pedestal/pedestal.tomcat "0.5.4"]
-
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.25"]
                  [kubernetes-api "1.4.0"]
@@ -26,8 +20,6 @@
                  [beamly/beamly-core.config "0.1.1"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
-  ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
-  ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.5"]]
   :repositories  [["central"  {:url "https://repo1.maven.org/maven2/" :snapshots false}]
                   ["clojars"  {:url "https://clojars.org/repo/"}]]
 
