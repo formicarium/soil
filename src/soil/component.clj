cdc4e3789acb6acc7d0702ecb0592a1c05c5e6d3
Rafael Leal <rafaelealdias@gmail.com>
2018-07-26 02:27:03 -0300

    Removes k8s-api-server on salvage tests

--- src/soil/component.clj
+++ src/soil/component.clj
@@ -7,12 +7,20 @@
             [soil.components.kubernetes.kubernetes-api-server :as kubernetes-api-server]
             [soil.components.configserver.configserver-client :as configserver-client]))
 
-(defn system-map
-  [env]
+(defn base [env]
   {:config         (config/new-config env)
    :service-map    (service/create-service env)
    :configserver   (component/using (configserver-client/new-configserver) [:config])
    :k8s-api-server (component/using (kubernetes-api-server/new-k8s-api-server) [:config])
    :k8s-client     (component/using (kubernetes-client/new-k8s-client) [:config :k8s-api-server])
    :soil-api       (component/using (soil-api/new-soil-api) [:k8s-client :service-map :configserver])})
+
+(defn test-system []
+  {:k8s-api-server {}})
+
+(defn system-map
+  [env]
+  (case env
+    "test" (merge (base env) (test-system))
+    (base env)))
 
