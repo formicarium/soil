50238a4af9f54d18052dd738850ea5cf373e0ba9
Leonardo Iacovini <leoiacovini@gmail.com>
2018-07-27 03:23:13 -0300

    Try to fix pedestal service map

--- src/soil/service.clj
+++ src/soil/service.clj
@@ -83,43 +83,11 @@
 
 ;; Consumed by soil.server/create-server
 ;; See http/default-interceptors for additional options you can configure
-(def service {:env                     :prod
-              ;; You can bring your own non-default interceptors. Make
-              ;; sure you include routing and set it up right for
-              ;; dev-mode. If you do, many other keys for configuring
-              ;; default interceptors will be ignored.
-              ;; ::http/interceptors []
-              ::http/routes            routes
-
-              ;; Uncomment next line to enable CORS support, add
-              ;; string(s) specifying scheme, host and port for
-              ;; allowed source(s):
-              ;;
-              ;; "http://localhost:8080"
-              ;;
-              ::http/allowed-origins   {:creds true :allowed-origins (constantly true)}
-              ::http/secure-headers    {:content-security-policy-settings {:object-src "none"}}
-
-              ;; Tune the Secure Headers
-              ;; and specifically the Content Security Policy appropriate to your service/application
-              ;; For more information, see: https://content-security-policy.com/
-              ;;   See also: https://github.com/pedestal/pedestal/issues/499
-              ;;::http/secure-headers {:content-security-policy-settings {:object-src "'none'"
-              ;;                                                          :script-src "'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:"
-              ;;                                                          :frame-ancestors "'none'"}}
-
-              ;; Root for resource interceptor that is available by default.
-              ::http/resource-path     "/public"
-
-              ;; Either :jetty, :immutant or :tomcat (see comments in project.clj)
-              ;;  This can also be your own chain provider/server-fn -- http://pedestal.io/reference/architecture-overview#_chain_provider
-              ::http/type              :jetty
-              ;;::http/host "localhost"
-              ::http/port              8080
-              ;; Options to pass to the container (Jetty)
-              ::http/container-options {:h2c? true
-                                        :h2?  false
-                                        :ssl? false}})
+(def service {:env                   :prod
+              ::http/routes          routes
+              ::http/allowed-origins {:creds true :allowed-origins (constantly true)}
+              ::http/type            :jetty
+              ::http/port            8080})
 
 (defn create-service
   [env]
