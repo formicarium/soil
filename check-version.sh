89ef51412d695433f23998c0ef9d9821f268143a
Leonardo Iacovini <leoiacovini@gmail.com>
2018-08-18 20:45:34 -0300

    Use right notation

--- check-version.sh
+++ check-version.sh
@@ -2,7 +2,7 @@
 
 set -eou pipefail
 
-version=$(cat ./resources/base.edn | grep  ':version .*"[0-9\.]*"' | grep -oEi '\d*\.\d*\.\d*')
+version=$(cat ./resources/base.edn | grep  ':version .*"[0-9\.]*"' | grep -oEi '[0-9]+\.[0-9]+\.[0-9]+')
 status_code=$(curl -s -o /dev/null -w "%{http_code}" https://hub.docker.com/v2/repositories/formicarium/soil/tags/${version}/)
 
 if [ ${status_code} -eq 200 ]; then
