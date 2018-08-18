48040a6e52450ad2f3a7ab8c256736204f88db3f
Leonardo Iacovini <leoiacovini@gmail.com>
2018-08-18 00:28:47 -0300

    Bump clj-service version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [leoiacovini/clj-service "1.2.0"]
+                 [leoiacovini/clj-service "1.3.0"]
                  [org.clojure/core.async "0.4.474"]
                  [selmer "1.11.9"]
                  [com.ibm.etcd/etcd-java "0.0.5"]
