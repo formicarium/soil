a8a926e22af58ce32109fc3bae548c085ac99656
Leonardo Iacovini <leoiacovini@gmail.com>
2018-08-17 23:42:45 -0300

    Set hostname on deployments

--- src/soil/schemas/application.clj
+++ src/soil/schemas/application.clj
@@ -4,10 +4,10 @@
 (s/defschema InterfaceType (s/enum :tcp :udp :http))
 (s/defschema ApplicationDefinition {:name       s/Str
                                     :devspace   s/Str
-                                    :containers [{:name      s/Str
-                                                  :image     s/Str
-                                                  :syncable? s/Bool
-                                                  :env       (s/pred map?)}]
+                                    :containers [{:name                       s/Str
+                                                  :image                      s/Str
+                                                  (s/optional-key :syncable?) s/Bool
+                                                  :env                        (s/pred map?)}]
                                     :interfaces [{:name      s/Str
                                                   :port      s/Int
                                                   :container s/Str
@@ -19,3 +19,4 @@
 (s/defschema Application {:name     s/Str
                           :devspace s/Str
                           :links    ApplicationUrls})
+
