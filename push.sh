4669aba4c7a8923d1f1cea22f9e3ebdf4dc9d2ee
Rafael Leal <rafaelealdias@gmail.com>
2018-08-20 23:31:49 -0300

    More fixes

---
+++ push.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+version=$(cat ./resources/base.edn | grep  ':version .*"[0-9\.]*"' | grep -oEi '[0-9]+\.[0-9]+\.[0-9]+')
+
+docker build -t soil:${version} .
+docker tag soil:${version} formicarium/soil:${version}
+docker tag soil:${version} formicarium/soil:latest
+docker push formicarium/soil:${version}
+docker push formicarium/soil:latest
