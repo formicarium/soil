41b3cf56f68d0cc1b0d2f93766f4f4de250bb228
Rafael Leal <rafaelealdias@gmail.com>
2018-09-20 09:48:40 -0300

    Empower kubernetes client

--- src/soil/components/kubernetes_client.clj
+++ src/soil/components/kubernetes_client.clj
@@ -9,10 +9,17 @@
             [kubernetes.api.extensions-v1beta1 :as extensions-v1beta1]
             [kubernetes.api.v1 :as k8s]
             [clj-service.exception :as exception]
+            [org.httpkit.client :as http]
             [clj-service.protocols.config :as protocols.config]))
 
 (def KubernetesContext {s/Keyword s/Any})
 (def ctx (k8s/make-context "http://localhost:9000"))
+
+(defn testing-logs [ctx pod-name namespace-name]
+  @(http/get (str (:server ctx) (str "/api/v1/namespaces/" namespace-name "/pods/" pod-name "/log"))
+            {:headers {"Authorization" (str "Bearer " (:token ctx))}
+             :as :stream
+             :insecure? true}))
 
 (s/defn get-config-map-impl
   [ctx :- KubernetesContext
@@ -34,11 +41,39 @@
 (s/defn create-namespace-impl!
   [ctx :- KubernetesContext
    k8s-namespace :- (s/pred map?)]
-  (<!! (k8s/create-namespace ctx k8s-namespace)))
+  (let [foo (<!! (k8s/create-namespace ctx k8s-namespace))]
+    (prn ctx foo)
+    foo))
 
 (s/defn delete-namespace-impl!
   [ctx :- KubernetesContext namespace-name :- s/Str]
   (<!! (k8s/delete-namespace ctx {} {:name namespace-name})))
+
+(s/defn finalize-namespace-impl!
+  [ctx :- KubernetesContext
+   namespace-name :- s/Str]
+  (<!! (k8s/replace-namespace-finalize ctx {:metadata {:name namespace-name}} {:name namespace-name})))
+
+(s/defn delete-all-deployments!
+  [ctx :- KubernetesContext
+   namespace-name :- s/Str]
+  (<!! (k8s-apps/deletecollection-namespaced-deployment ctx {:namespace namespace-name} {})))
+
+(s/defn list-all-services
+  [ctx :- KubernetesContext
+   namespace-name :- s/Str]
+  (:items (<!! (k8s/list-namespaced-service ctx {:namespace namespace-name}))))
+
+(s/defn delete-all-ingresses!
+  [ctx :- KubernetesContext
+   namespace-name :- s/Str]
+  (<!! (extensions-v1beta1/deletecollection-namespaced-ingress ctx {:namespace namespace-name})))
+
+(s/defn delete-service-account!
+  [ctx :- KubernetesContext
+   service-account-name :- s/Str
+   namespace-name :- s/Str]
+  (<!! (k8s/delete-namespaced-service-account ctx {} {:name service-account-name :namespace namespace-name})))
 
 (s/defn list-deployment-impl :- [(s/pred map?)]
   [ctx :- KubernetesContext
@@ -51,6 +86,18 @@
    deployment-namespace :- s/Str]
   (<!! (k8s-apps/delete-namespaced-deployment ctx {} {:name      deployment-name
                                                       :namespace deployment-namespace})))
+
+(s/defn get-pod-container-logs
+  [ctx :- KubernetesContext
+   pod-name :- s/Str
+   container-name :- s/Str
+   namespace-name :- s/Str
+   since-seconds :- (s/maybe s/Int)]
+  (<!! (k8s/read-namespaced-pod-log ctx (into {} (filter (comp some? val)
+                                                         {:name          pod-name
+                                                          :namespace     namespace-name
+                                                          :container     container-name
+                                                          :since-seconds since-seconds})))))
 
 (s/defn create-deployment-impl!
   [ctx :- KubernetesContext
@@ -96,6 +143,11 @@
    namespace :- s/Str]
   (:items (<!! (k8s/list-namespaced-pod ctx {:namespace namespace}))))
 
+(s/defn delete-all-services!
+  [ctx :- KubernetesContext
+   namespace-name :- s/Str]
+  (mapv #(delete-service-impl! ctx (get-in % [:metadata :name]) namespace-name) (list-all-services ctx namespace-name)))
+
 (defn check-api-health
   [ctx]
   (let [api-resources (<!! (k8s/get-api-resources ctx))]
@@ -124,12 +176,18 @@
   (delete-namespace! [this namespace-name]
     (-> (delete-namespace-impl! (:ctx this) namespace-name)
         (raise-errors!)))
+  (finalize-namespace! [this namespace-name]
+    (-> (finalize-namespace-impl! (:ctx this) namespace-name)
+        (raise-errors!)))
 
   (create-ingress! [this ingress]
     (-> (create-ingress-impl! (:ctx this) ingress)
         (raise-errors!)))
   (delete-ingress! [this ingress-name namespace]
     (-> (delete-ingress-impl! (:ctx this) ingress-name namespace)
+        (raise-errors!)))
+  (delete-all-ingresses! [this namespace]
+    (-> (delete-all-ingresses! (:ctx this) namespace)
         (raise-errors!)))
 
   (create-service! [this service]
@@ -141,6 +199,8 @@
   (delete-service! [this service-name namespace]
     (-> (delete-service-impl! (:ctx this) service-name namespace)
         (raise-errors!)))
+  (delete-all-services! [this namespace]
+    (delete-all-services! (:ctx this) namespace))
 
   (create-deployment! [this deployment]
     (-> (create-deployment-impl! (:ctx this) deployment)
@@ -150,6 +210,13 @@
         (raise-errors!)))
   (delete-deployment! [this deployment-name namespace]
     (-> (delete-deployment-impl! (:ctx this) deployment-name namespace)
+        (raise-errors!)))
+  (delete-all-deployments! [this namespace]
+    (-> (delete-all-deployments! (:ctx this) namespace)
+        (raise-errors!)))
+
+  (delete-service-account! [this service-account-name namespace]
+    (-> (delete-service-account! (:ctx this) service-account-name namespace)
         (raise-errors!)))
 
   (get-config-map [this name namespace]
--- src/soil/protocols/kubernetes_client.clj
+++ src/soil/protocols/kubernetes_client.clj
@@ -6,21 +6,27 @@
   (create-namespace! [this k8s-namespace]                     "Creates a Kubernetes namespace")
   (list-namespaces [this]                                     "List Kubernetes namespaces")
   (delete-namespace! [this namespace-name]                    "Deletes a Kubernetes namespace")
+  (finalize-namespace! [this namespace-name]                  "Finalizes a Kubernetes namespace")
 
   (create-ingress! [this ingress]                             "Creates a Kubernetes ingress")
   (delete-ingress! [this ingress-name namespace]              "Delete a Kubernetes ingress")
+  (delete-all-ingresses! [this namespace]                     "Delete all Kubernetes ingresses in a namespace")
 
   (create-service! [this service]                             "Creates a Kubernetes Service")
   (get-service [this service-name namespace]                  "Get a Kubernetes Service")
   (delete-service! [this service-name namespace]              "Delete a Kubernetes Service")
+  (delete-all-services! [this namespace]                      "Delete all Kubernetes services in a namespace")
 
-  (create-deployment! [this deployment]                       "Creates a Kubernetes deployment")
+  (create-deployment! [this deployment]                        "Creates a Kubernetes deployment")
   (list-deployment [this deployment]                           "List Kubernetes deployments")
-  (delete-deployment! [this deployment-name namespace]        "Deletes a Kubernetes deployment")
+  (delete-deployment! [this deployment-name namespace]         "Deletes a Kubernetes deployment")
+  (delete-all-deployments! [this namespace]                    "Delete all Kubernetes deployments in a namespace")
 
-  (get-config-map [this name namespace]                       "Get a Kubernetes ConfigMap")
-  (patch-config-map! [this name namespace config-map]         "Patch a Kubernetes ConfigMap")
+  (delete-service-account! [this service-account-name namespace] "Delete a Kubernetes ServiceAccount")
 
-  (list-pods [this namespace]                                 "List Kubernetes Pods"))
+  (get-config-map [this name namespace]                        "Get a Kubernetes ConfigMap")
+  (patch-config-map! [this name namespace config-map]          "Patch a Kubernetes ConfigMap")
+
+  (list-pods [this namespace]                                  "List Kubernetes Pods"))
 
 (def IKubernetesClient (:on-interface KubernetesClient))
