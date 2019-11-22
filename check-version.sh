bfa35228aa5b7da817517a41053ff89210bc9d72
Rafael Leal <rafaelealdias@gmail.com>
2019-11-22 15:53:38 -0300

    Adds beta branch

--- check-version.sh
+++ check-version.sh
@@ -2,7 +2,7 @@
 
 set -eou pipefail
 
-version=$(cat ./resources/base.edn | grep  ':version .*"[0-9\.]*"' | grep -oEi '[0-9]+\.[0-9]+\.[0-9]+')
+version=$(cat ./resources/base.edn | grep  ':version .*"[-0-9\.a-zA-Z]*"' | grep -oEi '[0-9]+\.[0-9]+\.[0-9]+-beta[0-9]*')
 status_code=$(curl -s -o /dev/null -w "%{http_code}" https://hub.docker.com/v2/repositories/formicarium/soil/tags/${version}/)
 
 if [ ${status_code} -eq 200 ]; then
