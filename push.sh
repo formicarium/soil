3583bee6c1f4cc482a41c19b12fa9289f0598187
Rafael Leal <rafaelealdias@gmail.com>
2019-11-22 15:58:02 -0300

    Fix push script for beta

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 set -eou pipefail
 
-version=$(cat ./resources/base.edn | grep  ':version .*"[0-9\.]*"' | grep -oEi '[0-9]+\.[0-9]+\.[0-9]+')
+version=$(cat ./resources/base.edn | grep  ':version .*"[-0-9\.a-zA-Z]*"' | grep -oEi '[0-9]+\.[0-9]+\.[0-9]+-beta[0-9]*')
 docker build -t soil:${version} .
 docker tag soil:${version} formicarium/soil:${version}
 docker tag soil:${version} formicarium/soil:latest
